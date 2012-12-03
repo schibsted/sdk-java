a80e5ea2d82be499f8c4b978d766368b0b431e49
mikaellindstrom <mikael.lindstrom@netlight.com>
2012-12-03 12:26:09 +0100

    * Added user_id to OauthCredentials

--- sdk/src/main/java/no/spp/sdk/oauth/OauthCredentials.java
+++ sdk/src/main/java/no/spp/sdk/oauth/OauthCredentials.java
@@ -1,29 +1,36 @@
 package no.spp.sdk.oauth;
 
 import net.smartam.leeloo.client.response.OAuthAccessTokenResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
 import java.util.Date;
 
 public class OauthCredentials implements Serializable {
 
+    private static final Logger log = LoggerFactory.getLogger(OauthCredentials.class);
+
     private final Date expiresDate;
+    private final String userId;
     private final String accessToken;
     private final String refreshToken;
     private final String expiresInSeconds;
 
-    public OauthCredentials(String accessToken, String refreshToken, String expiresInSeconds){
+    public OauthCredentials(String accessToken, String refreshToken, String expiresInSeconds, String userId){
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
         this.expiresInSeconds = expiresInSeconds;
-        expiresDate = getExpiryDateFromExpiresIn(expiresInSeconds);
+        this.expiresDate = getExpiryDateFromExpiresIn(expiresInSeconds);
+        this.userId = userId;
     }
 
     public OauthCredentials(OAuthAccessTokenResponse oAuthAccessTokenResponse){
-        accessToken = oAuthAccessTokenResponse.getAccessToken();
-        refreshToken = oAuthAccessTokenResponse.getRefreshToken();
-        expiresInSeconds = oAuthAccessTokenResponse.getExpiresIn();
-        expiresDate = getExpiryDateFromExpiresIn(expiresInSeconds);
+        this.accessToken = oAuthAccessTokenResponse.getAccessToken();
+        this.refreshToken = oAuthAccessTokenResponse.getRefreshToken();
+        this.expiresInSeconds = oAuthAccessTokenResponse.getExpiresIn();
+        this.expiresDate = getExpiryDateFromExpiresIn(expiresInSeconds);
+        this.userId = oAuthAccessTokenResponse.getParam("user_id");
     }
 
     private Date getExpiryDateFromExpiresIn(String expiresInSeconds) {
@@ -51,10 +58,15 @@ public class OauthCredentials implements Serializable {
         return expiresDate.before(when);
     }
 
+    public String getUserId() {
+        return this.userId;
+    }
+
     @Override
     public String toString() {
         return "OauthCredentials{" +
                 "expires=" + expiresDate +
+                ", userId='" + userId + '\'' +
                 ", accessToken='" + accessToken + '\'' +
                 ", refreshToken='" + refreshToken + '\'' +
                 ", expiresInSeconds='" + expiresInSeconds + '\'' +
