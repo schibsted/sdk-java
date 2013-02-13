76436669c2195bea2501096b25c33470b2cd419f
Fredrik Forsberg <ff@kth.se>
2013-02-13 11:55:48 +0100

    OAuthAccessTokenResponse property userID exposed in class OauthCredentials

--- sdk/src/test/java/no/spp/sdk/oauth/OauthCredentialsTest.java
+++ sdk/src/test/java/no/spp/sdk/oauth/OauthCredentialsTest.java
@@ -8,6 +8,7 @@ import net.smartam.leeloo.common.exception.OAuthSystemException;
 import no.spp.sdk.oauth.OauthCredentials;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -16,15 +17,16 @@ public class OauthCredentialsTest {
 
     private OAuthAccessTokenResponse resp;
     private OauthCredentials oauthCredentials;
+    private static final String USER_ID = "100001";
 
-    private OAuthAccessTokenResponse getOAuthAccessTokenResponseWithExpires(int expires) throws OAuthSystemException, OAuthProblemException {
-        String s = "{\"access_token\":\"211b54aaf8e9c36311b9c3fb5773e4a2e314c2c1\",\"expires_in\":" + expires + ",\"scope\":null,\"user_id\":false,\"refresh_token\":\"d7fbd1482eff47954feac77d70994036ee597a09\",\"server_time\":1320127907}";
+    private OAuthAccessTokenResponse getOAuthAccessTokenResponseWithExpires(int expires, String userId) throws OAuthSystemException, OAuthProblemException {
+        String s = "{\"access_token\":\"211b54aaf8e9c36311b9c3fb5773e4a2e314c2c1\",\"expires_in\":" + expires + ",\"scope\":null,\"user_id\": \"" + userId + "\",\"refresh_token\":\"d7fbd1482eff47954feac77d70994036ee597a09\",\"server_time\":1320127907}";
         return OAuthClientResponseFactory.createCustomResponse( s, "application/json", 200, OAuthJSONAccessTokenResponse.class);
     }
 
     @Before
     public void setUp() throws OAuthProblemException, OAuthSystemException {
-        oauthCredentials = new OauthCredentials(getOAuthAccessTokenResponseWithExpires(3600) );
+        oauthCredentials = new OauthCredentials(getOAuthAccessTokenResponseWithExpires(3600, USER_ID) );
     }
     @Test
     public void returnsCorrectAccessToken() throws Exception {
@@ -47,8 +49,12 @@ public class OauthCredentialsTest {
     }
     @Test
     public void whenExpiresIn0CredentialsIsNotExpiredNow() throws Exception {
-         oauthCredentials = new OauthCredentials(getOAuthAccessTokenResponseWithExpires(0));
+         oauthCredentials = new OauthCredentials(getOAuthAccessTokenResponseWithExpires(0, USER_ID));
          assertTrue("Should be expired", oauthCredentials.isExpiredNow());
     }
 
+    @Test
+    public void returnsCorrectUserId() throws Exception {
+        assertEquals(oauthCredentials.getUserId(), USER_ID);
+    }
 }
